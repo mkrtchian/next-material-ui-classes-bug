1a7c5e21b5506559f471705de61549558c192e74
Roman Mkrtchian <mkrtchia@gmail.com>
2021-03-01 12:36:12 +0100

    feat: use router push to test behavior instead of a normal link

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import ProTip from "../src/ProTip";
+import Link from "../src/Link";
+import Copyright from "../src/Copyright";
+import { useRouter } from "next/router";
 
 export default function Index() {
+  const router = useRouter();
+  const handleClick = (e) => {
+    e.preventDefault();
+    router.push("/about");
+  };
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
+        <p>
+          <Link href="/about" color="secondary">
+            Go to the about page
+          </Link>
+        </p>
+        <p>
+          <button onClick={handleClick}>
+            Go to the about page with router.push
+          </button>
+        </p>
         <ProTip />
         <Copyright />
       </Box>
